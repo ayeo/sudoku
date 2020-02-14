073f358dcbb0e4e8e40a32bd71956321d44aab7d
ayeo <me@ayeo.pl>
2020-02-14 16:18:14 +0100

    Dev

--- src/main/scala/Window.scala
+++ src/main/scala/Window.scala
@@ -1,17 +1,31 @@
-import javafx.application.Application
-import javafx.stage.Stage
+import scalafx.application.JFXApp
+import scalafx.stage.Stage
+import scalafx.event.ActionEvent
 import scalafx.scene.Scene
 import scalafx.scene.control.Button
-import scalafx.scene.layout.BorderPane
+import scalafx.scene.layout.GridPane
+import scalafx.Includes._
 
-class Window extends Application {
-  var btn: Button = new Button()
+object Window extends JFXApp {
+  stage = new JFXApp.PrimaryStage
+  start(stage)
+  
+  def start(primaryStage: Stage): Unit = {
+    val pane: GridPane = new GridPane()
+    val scene: Scene = new Scene(pane, 500, 500)
 
-  override def start(primaryStage: Stage): Unit = {
-    btn.setText("Click me")
-    val pane: BorderPane = new BorderPane()
-    pane.setCenter(btn)
-    val scene: Scene = new Scene(pane, 300, 300)
+    (1 to 9).foreach(row => {
+      (1 to 9).foreach(column => {
+        val btn = new Button()
+        btn.setText("0")
+        btn.setId(s"cell_${row*column}")
+        pane.add(btn, row, column)
+        btn.onAction = (event: ActionEvent) => {
+          println("Bomber")
+        }
+      })
+    })
+
     primaryStage.setScene(scene)
     primaryStage.show()
   }
