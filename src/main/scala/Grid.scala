a3bc153f7345bbfa76ab49c6b98f7c7aacb7ee0c
ayeo <me@ayeo.pl>
2020-02-29 03:47:13 +0100

    Grid poc

--- src/main/scala/Grid.scala
+++ src/main/scala/Grid.scala
@@ -2,44 +2,63 @@ import scalafx.application.JFXApp
 import scalafx.geometry.Insets
 import scalafx.scene.canvas.Canvas
 import scalafx.scene.control.Button
-import scalafx.scene.layout.{BorderPane, GridPane}
+import scalafx.scene.layout.{BorderPane, GridPane, Pane}
 import scalafx.scene.paint.Color
 import scalafx.scene.Scene
 import scala.collection.mutable.Seq
 
-object Grid extends JFXApp {
-  val canvas = new Canvas(452, 452)
-  val gc = canvas.graphicsContext2D
-
-  val w = 1
-  gc.stroke = Color.Black
-  gc.lineWidth = w
-  (0 to 9).foreach(row => {
-    gc.strokeLine(50 * row + w, w, 50 * row + w, 450 + w)
-    gc.strokeLine(w, 50 * row + w, 450 + w, 50 * row + w)
-  })
-
-  gc.lineWidth = w * 2
-  (0 to 3).foreach(row => {
-    gc.strokeLine(150 * row + w, w, 150 * row + w, 450 + w)
-    gc.strokeLine(w, 150 * row + w, 450 + w, 150 * row + w)
-  })
-
-  val mainPane: BorderPane = new BorderPane()
-  val scene: Scene = new Scene(mainPane, 500, 500)
-
-  val boardPane = new BorderPane()
-  val pane: GridPane = new GridPane()
-  pane.padding = Insets(w, w, w, w)
-  boardPane.setCenter(pane)
-  boardPane.children.add(canvas)
-
-  mainPane.padding = Insets(10, 10, 10, 10)
-  mainPane.setCenter(boardPane)
-
-
+object BoardBuilder {
+  type Board = Seq[Seq[Int]]
   val cells = Array.ofDim[Button](9, 9)
-  val board: Seq[Seq[Int]] = Seq(
+
+  def buildBoard(data: Board, cellSize: Int, borderSize: Int): Pane = {
+    val boardPane = new BorderPane()
+    val pane: GridPane = new GridPane()
+
+    pane.padding = Insets(borderSize, borderSize, borderSize, borderSize)
+    boardPane.setCenter(pane)
+    boardPane.children.add(BoardBuilder.buildCanvas(cellSize, borderSize))
+    (0 to 8).foreach(row => {
+      (0 to 8).foreach(column => {
+        val btn = new Button()
+        cells(column)(row) = btn
+        if (data(column)(row ) > 0) {
+          btn.setText(data(column)(row).toString())
+        }
+
+        btn.setMinSize(cellSize, cellSize)
+        btn.setMaxSize(cellSize, cellSize)
+        btn.setStyle(s"-fx-background-color: #FFFFFF; -fx-font-size: ${cellSize/2};")
+        btn.setId(s"cell_${row * column}")
+        pane.add(btn, row, column)
+      })
+    })
+
+    return boardPane
+  }
+
+  private def buildCanvas(size: Int, borderSize: Int): Canvas = {
+    val canvas = new Canvas(9 * size + 2 * borderSize, 9 * size + 2 * borderSize)
+    val gc = canvas.graphicsContext2D
+    gc.stroke = Color.Black
+    gc.lineWidth = borderSize
+    (0 to 9).foreach(row => {
+      gc.strokeLine(size * row + borderSize, borderSize, size * row + borderSize, size * 9 + borderSize)
+      gc.strokeLine(borderSize, size * row + borderSize, size * 9 + borderSize, size * row + borderSize)
+    })
+
+    gc.lineWidth = borderSize * 2
+    (0 to 3).foreach(row => {
+      gc.strokeLine(size * 3 * row + borderSize, borderSize, size * 3  * row + borderSize, size * 9 + borderSize)
+      gc.strokeLine(borderSize, size * 3 * row + borderSize, size * 9 + borderSize, size * 3 * row + borderSize)
+    })
+
+    return canvas
+  }
+}
+
+object Grid extends JFXApp {
+  val board: BoardBuilder.Board = Seq(
     Seq(8, 0, 0, 0, 0, 7, 0, 9, 0),
     Seq(0, 7, 0, 0, 2, 0, 0, 0, 8),
     Seq(0, 0, 9, 6, 0, 0, 5, 0, 0),
@@ -51,24 +70,10 @@ object Grid extends JFXApp {
     Seq(0, 0, 7, 0, 0, 0, 3, 0, 0)
   )
 
-  (0 to 8).foreach(row => {
-    (0 to 8).foreach(column => {
-      val btn = new Button()
-      cells(column)(row) = btn
-      if (board(column)(row ) > 0) {
-        btn.setText(board(column)(row).toString())
-      }
-
-      btn.setMinSize(50, 50)
-      btn.setMaxSize(50, 50)
-      btn.setStyle("-fx-background-color: #FFFFFF; -fx-font-size: 24;")
-      btn.setId(s"cell_${row * column}")
-
-      pane.add(btn, row, column)
-    })
-  })
-
-
+  val mainPane: BorderPane = new BorderPane()
+  val scene: Scene = new Scene(mainPane, 400, 400)
+  mainPane.padding = Insets(10, 10, 10, 10)
+  mainPane.setCenter(BoardBuilder.buildBoard(board, 40, 1))
 
   stage = new JFXApp.PrimaryStage
   stage.setScene(scene)
