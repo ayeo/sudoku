0f4a6df42146aa7087a2f67f62478b0c344cc927
ayeo <me@ayeo.pl>
2020-03-01 21:03:07 +0100

    Use Option

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -11,21 +11,20 @@ import scalafx.Includes._
 object Game extends JFXApp {
   val mainPane: BorderPane = new BorderPane()
   val scene: Scene = new Scene(mainPane, 400, 400)
-
   val loader = new Loader(Levels.EASY)
-  val board: Domain.Board = loader.loadRandomPuzzle
+
+  def loadNewBoard(): Unit = {
+    val board = loader.loadRandomPuzzle.getOrElse(throw new RuntimeException)
+    mainPane.setCenter(BoardBuilder.buildBoard(board, 40, 1))
+  }
 
   mainPane.padding = Insets(10, 10, 10, 10)
-  mainPane.setCenter(BoardBuilder.buildBoard(board, 40, 1))
+  loadNewBoard()
 
   val newGame = new Button()
   newGame.setText("New game")
   mainPane.setRight(newGame)
-  newGame.onAction = (event: ActionEvent) => {
-    val board: Domain.Board = loader.loadRandomPuzzle
-    mainPane.setCenter(BoardBuilder.buildBoard(board, 40, 1))
-  }
-
+  newGame.onAction = (event: ActionEvent) => loadNewBoard()
   stage = new JFXApp.PrimaryStage
   stage.setScene(scene)
   stage.show()
--- src/main/scala/Loader.scala
+++ src/main/scala/Loader.scala
@@ -5,14 +5,21 @@ import scala.util.Random
 class Loader(level: Levels.Level) {
   private lazy val puzzles = init(level)
 
-  private def init(level: Levels.Level): List[String] = {
-    val data = io.Source.fromFile(s"resources/${level.toString}.txt")
-    val result = data.getLines.toList
-    data.close
-    result
+  private def init(level: Levels.Level): Option[List[String]] = {
+    try {
+      val data = io.Source.fromFile(s"resources/${level.toString}.txt")
+      val result = data.getLines.toList.filter(p => p.length == 81)
+      data.close
+      Some(result)
+    } catch {
+      case _ => None
+    }
   }
 
-  def loadRandomPuzzle: Domain.Board = {
-    puzzles.drop(Random.nextInt(puzzles.size)).head.toVector.map(_.toInt - 48).sliding(9, 9).toVector
+  def loadRandomPuzzle: Option[Domain.Board] = {
+    puzzles.map(list => {
+      val position = Random.nextInt(list.size)
+      list.drop(position).head.toVector.map(_.toInt - 48).sliding(9, 9).toVector
+    })
   }
 }
